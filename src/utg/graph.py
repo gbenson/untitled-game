5fa7bad839bb49d72e7472ab13432e2d1c43342a
Gary Benson <gary@gbenson.net>
2023-03-29 00:12:23 +0100

    Document triangulation algorithm

--- src/utg/graph.py
+++ src/utg/graph.py
@@ -30,3 +30,26 @@ class Graph:
         min_y = min(node.y for node in self.nodes)
         max_y = max(node.y for node in self.nodes)
         return Node(((min_x + max_x) // 2, (min_y + max_y) // 2))
+
+    def triangulate(self):
+        """Triangulate the nodes using the the S-hull algorithm.
+        """
+        # For a set of unique points xi in R2:
+        #
+        #  1. Select a seed point x0 from xi.
+        #  2. Sort according to |xi − x0|**2
+        #  3. Find the point xj closest to x0.
+        #  4. Find the point xk that creates the smallest circumcircle
+        #     with x0 and xj and record the center of the circumcircle
+        #     as C.
+        #  5. Order points [x0, xj, xk] to give a right handed system.
+        #     This is the initial seed convex hull.
+        #  6. Re-sort the remaining points according to |xi − C|**2
+        #     to give points si.
+        #  7. Sequentially add the points si to the propagating convex
+        #     hull.  As each new point is added, the facets of the hull
+        #     that are visible to it form new triangles.
+        #  8. A non-overlapping triangulation of the set of points has
+        #     now been created.  Adjacent pairs of triangles of this
+        #     triangulation must be ’flipped’ to create a Delaunay
+        #     triangulation.
