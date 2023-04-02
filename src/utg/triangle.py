2c30df4eeeceb5b8f6045eb78ea1ef3c4954fb61
Gary Benson <gary@gbenson.net>
2023-04-02 14:47:17 +0100

    Introduce is_circumcentre_within_triangle()

--- src/utg/triangle.py
+++ src/utg/triangle.py
@@ -14,3 +14,27 @@ def circumcentre(a, b, c):
                   oOD * (ax2Pay2 * (c.x - b.x) +
                          + bx2Pby2 * (a.x - c.x)
                          + cx2Pcy2 * (b.x - a.x))))
+
+# For an acute triangle (all angles smaller than a right angle), the
+# circumcenter always lies inside the triangle.
+#
+# For a right triangle, the circumcenter always lies at the midpoint
+# of the hypotenuse.
+#
+# For an obtuse triangle (a triangle with one angle bigger than a
+# right angle), the circumcenter always lies outside the triangle.
+#
+# https://en.wikipedia.org/wiki/Circumscribed_circle#Location_relative_to_the_triangle
+#
+# When two vectors are at right angles to each other the dot product is zero.
+
+# https://www.mathsisfun.com/algebra/vectors-dot-product.html
+def is_circumcentre_within_triangle(a, b, c):
+    for p, q, r in ((c, a, b), (a, b, c), (b, c, a)):
+        qpx = p.x - q.x
+        qpy = p.y - q.y
+        qrx = r.x - q.x
+        qry = r.y - q.y
+        if qpx * qrx + qpy * qry <= 0:
+            return False
+    return True
