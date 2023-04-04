10219b5606bb6778822687204bc5171a7bddcd10
Gary Benson <gary@gbenson.net>
2023-04-04 22:09:43 +0100

    Paged test?

--- src/utg/devel/maze_driver.py
+++ src/utg/devel/maze_driver.py
@@ -19,17 +19,60 @@ TESTNODES = (
     (458, 407), (27, 415), (308, 433), (112, 466),
 )
 
-def testnodes(N=64):
+
+if True:
+    num_rows = 8
+    num_cols = 4
+    page_width = 30
+    page_height = 17
+
+    x_clear = 2
+    y_clear = 2
+    nodes_per_page = 6
+
+    map_width = page_width * num_cols
+    map_height = page_height * num_rows
+
+    scale = 10
+
+
+def testnodes():
     random.seed(186283)
-    for _ in range(N):
-        yield random.randrange(10, 491), random.randrange(10, 491)
+    for row in range(num_rows):
+        ymin = row * page_height
+        ymax = ymin + page_height
+        ymin += y_clear
+        ymax -= y_clear
+        for col in range(num_cols):
+            xmin = col * page_width
+            xmax = xmin + page_width
+            xmin += x_clear
+            xmax -= x_clear
+            for _ in range(nodes_per_page):
+                yield (random.randrange(xmin, xmax),
+                       random.randrange(ymin, ymax))
+
+def clear(screen):
+    for row in range(num_rows):
+        ymin = row * page_height
+        for col in range(num_cols):
+            if row & 1 == col & 1:
+                continue
+            xmin = col * page_width
+            screen.fill("#eeeeee",
+                        pygame.Rect(
+                            [d * scale
+                             for d in (xmin, ymin,
+                                       page_width,
+                                       page_height)]))
+
 
 def main():
-    maze = Maze(testnodes())  # TESTNODES)
+    maze = Maze(testnodes())
     solver = CaveSolver(maze)
 
     pygame.init()
-    screen = pygame.display.set_mode((1000, 1000))
+    screen = pygame.display.set_mode((120 * scale, 136 * scale))
     clock = pygame.time.Clock()
 
     while True:
@@ -47,22 +90,23 @@ def main():
             pass
 
         screen.fill("white")
+        clear(screen)
         draw_graph(screen, maze.path_graph, "gold")
         draw_graph(screen, maze.wall_graph, "navy")
         pygame.display.flip()  # Refresh on-screen display
         clock.tick(20)  # wait until next frame (at 60 FPS)
 
-def draw_graph(surface, graph, color, scale=2):
+def draw_graph(surface, graph, color):
     for edge in graph.edges:
         if not edge.visible:
             continue
         start, end = [(v.x * scale, v.y*scale) for v in edge.vertices]
         pygame.draw.line(surface,
                          "red" if edge.visited else color,
-                         start, end, width=2*scale)
+                         start, end, width=5)
     for vertex in graph.vertices:
         pygame.draw.circle(surface,
                            "red" if vertex.visited else color,
                            (vertex.x * scale,
                             vertex.y * scale),
-                           radius=3 * scale)
+                           radius=5)
